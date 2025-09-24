0462e95f3f32da94ed57c46508be1b4a056a1a4b
nyassky <gswarrios@gmail.com>
2025-09-24 13:22:41 +0300

    реализован module2-task1

---
+++ src/components/offer_card.tsx
@@ -0,0 +1,51 @@
+type OfferCardProps = {
+	isPremium: boolean;
+	imgUrl: string;
+	cost: number;
+	dayOrNight: string;
+	isBookmarks: string;
+	name: string;
+	typeOfApartment: string;
+}
+
+function OfferCard({isPremium, imgUrl, cost, dayOrNight, isBookmarks, name, typeOfApartment} : OfferCardProps) {
+	return (
+		<article className="cities__card place-card">
+		{isPremium ?
+			<div className="place-card__mark">
+				<span>Premium</span>
+			</div>
+		: null}
+		<div className="cities__image-wrapper place-card__image-wrapper">
+			<a href="#">
+			<img className="place-card__image" src={imgUrl} width="260" height="200" alt="Place image"/>
+			</a>
+		</div>
+		<div className="place-card__info">
+		<div className="place-card__price-wrapper">
+			<div className="place-card__price">
+				<b className="place-card__price-value">&euro;{cost}</b>
+				<span className="place-card__price-text">&#47;&nbsp;{dayOrNight}</span>
+			</div>
+			<button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+				<svg className="place-card__bookmark-icon" width="18" height="19">
+				<use xlinkHref="#icon-bookmark"></use>
+				</svg>
+				<span className="visually-hidden">{isBookmarks}</span>
+			</button>
+		</div>
+			<div className="place-card__rating rating">
+			<div className="place-card__stars rating__stars">
+				<span className="width: 80%"></span>
+				<span className="visually-hidden">Rating</span>
+			</div>
+			</div>
+			<h2 className="place-card__name">
+			<a href="#">{name}</a>
+			</h2>
+			<p className="place-card__type">{typeOfApartment}</p>
+		</div>
+		</article>
+	);
+}
+export default OfferCard;
